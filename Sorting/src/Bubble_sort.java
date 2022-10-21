307829a2fbd5a2d0486a1106ff5344cf0965854b
AhnMinsur <minwisdom08@gmail.com>
2022-10-21 11:17:41 +0530

    Done Bubble Sorting in another way

--- Sorting/src/Bubble_sort.java
+++ Sorting/src/Bubble_sort.java
@@ -2,17 +2,24 @@ public class Bubble_sort {
 
     public static void main(String args[]) {
         int[] arr = new int[] {3, 1, 4, 6, 7, 2, 5};
-        for ( int i = 0; i < arr.length-1; i++ ) {
-            for ( int j = 0; j < arr.length-1; j++ ) {
-                if ( arr[j] > arr[j+1] ) {
-                    int a = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = a;
+        while ( true ) {
+            int count = 0;
+            for ( int i = 0; i < arr.length-1; i++ ) {
+                if ( arr[i] > arr[i+1] ) {
+                    int a = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = a;
+                }
+                else {
+                    count++;
                 }
             }
+            if ( count == arr.length - 1 ) {
+                break;
+            }
         }
-        for (int i = 0; i < arr.length; i++ ) {
-            System.out.print(arr[i]);
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j]);
         }
     }
 }
