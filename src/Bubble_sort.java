3443dcc497f9f81dc9536f71a926224d3d613af5
AhnMinsur <minwisdom08@gmail.com>
2022-10-21 07:23:38 +0530

    Update Bubble_sort.java
    Bubble Sort complete

--- src/Bubble_sort.java
+++ src/Bubble_sort.java
@@ -1,6 +1,26 @@
 public class Bubble_sort {
 
     public static void main(String args[]) {
-        System.out.println();
+        int[] arr = new int[] {3, 1, 4, 6, 7, 2, 5};
+        while ( true ) {
+            int count = 0;
+            for ( int i = 0; i < arr.length-1; i++ ) {
+                if ( arr[i] > arr[i+1] ) {
+                    int a = arr[i];
+                    int b = arr[i+1];
+                    arr[i+1] = a;
+                    arr[i] = b;
+                }
+                else {
+                    count++;
+                }
+            }
+            if ( count == arr.length - 1 ) {
+                for (int j : arr) {
+                    System.out.print(j);
+                }
+                return;
+            }
+        }
     }
 }
