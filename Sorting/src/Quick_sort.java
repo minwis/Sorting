2b452d1257be775033f7107252f66a53c98fb918
AhnMinsur <minwisdom08@gmail.com>
2022-10-22 22:09:54 +0530

    made sorting algorithm
    need some revise, but the code is ready for recursion

--- Sorting/src/Quick_sort.java
+++ Sorting/src/Quick_sort.java
@@ -1,38 +1,44 @@
 public class Quick_sort {
 
-    public static void QuickSort(int[] arr) {
-        int half_len = (arr.length - (arr.length % 2)) / 2;
-        int pivot = arr[half_len - 1];
+    public static int[] QuickSort(int[] arr) {
+        int pivot_position = (arr.length - 1) / 2;
+        int pivot = arr[pivot_position];
+        arr[pivot_position] = arr[arr.length - 1];
+        arr[arr.length - 1] = pivot;
+        int i = 0, j = 0;
 
-        for ( int i = 0; i <= half_len; i++ ) {
-
-            //NfromLeft와 NfromLeft를 바꾼다.
-            int NfromLeft_position = 0;
-            int NfromRight_position = 0;
-            for (int j = 0; j < half_len; j++ ) {
-                if ( arr[j] >= pivot ) {
-                    NfromLeft_position = j;
-                    break;
-                }
+        while ( i + j < arr.length / 2) {
+            while ( arr[i] < pivot ) { //pivot보다 큰 수를 뒤로 보내야 함.
+                i++;
             }
-            for (int j = arr.length - 1; j >= half_len; j-- ) {
-                if ( arr[j] <= pivot ) {
-                    NfromRight_position = j;
-                    break;
-                }
+            while ( arr[arr.length - 2 - j] >= pivot ) {
+                j++;
             }
-            int a = arr[NfromLeft_position];
-            arr[NfromLeft_position] = arr[NfromRight_position];
-            arr[NfromRight_position] = a;
+            int a = arr[i];
+            arr[i] = arr[arr.length - 2 - j];
+            arr[arr.length - 2 - j] = a;
         }
 
-        System.out.println("no error");
-
+        for ( int I = 0; I < arr.length; I++ ) {
+            if ( arr[I] > pivot ) {
+                int backup = arr[I];
+                arr[I] = pivot;
+                arr[arr.length - 1] = backup;
+                break;
+            }
+        }
+        return arr;
     }
 
     public static void main(String args[]) {
-        int[] arr = new int[] {8,1,6,9,6,3,5,2,7,0};;
-        QuickSort(arr);
+        int[] arr = new int[] {8,1,6,9,6,3,5,2,7,0};
+        System.out.println("Input: \n8169635270\n");
+        System.out.println("Purposed to: \n2153066879\n\nOutput:");
+        arr = QuickSort(arr);
+        for ( int i = 0; i < arr.length; i++ ) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
     }
 
 }
