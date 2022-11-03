122dea30758410c91a6ad467a8ddb08dfa18c750
AhnMinsur <minwisdom08@gmail.com>
2022-11-03 10:11:07 +0530

    Timer

--- Sorting/src/Bubble_sort.java
+++ Sorting/src/Bubble_sort.java
@@ -1,7 +1,7 @@
 public class Bubble_sort {
 
     public static void main(String args[]) {
-        int[] arr = new int[] {3, 1, 4, 6, 7, 2, 5};
+        int[] arr = new int[] {9,-3,5,2,3,8,-6,1,6};
         for ( int i = 0; i < arr.length-1; i++ ) {
             for ( int j = i+1; j < arr.length; j++ ) {
                 if ( arr[i] > arr[j] ) {
@@ -13,6 +13,9 @@ public class Bubble_sort {
         }
         for (int i = 0; i < arr.length; i++ ) {
             System.out.print(arr[i]);
+            if ( i != arr.length - 1 ) {
+                System.out.print(",");
+            }
         }
     }
 }
--- Sorting/src/Compare.java
+++ Sorting/src/Compare.java
@@ -1,6 +1,14 @@
 public class Compare {
 
     public static void main(String[] args) {
-        
+        long start1 = System.nanoTime();
+        new Bubble_sort();
+        long end1 = System.nanoTime();
+        System.out.println("Bubble Sort: " + (end1-start1));
+
+        long start2 = System.nanoTime();
+        new Quick_sort();
+        long end2 = System.nanoTime();
+        System.out.println("Quick Sort: " + (end2-start2));
     }
 }
